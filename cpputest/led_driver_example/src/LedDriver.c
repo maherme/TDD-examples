33afb9261e64d71e94ea62cf95b9b4069057ef44
maherme <manuel.hernandez.mendez@gmail.com>
2023-08-10 20:49:56 +0200

    Add more tests to cpputest/led_driver_example

--- cpputest/led_driver_example/src/LedDriver.c
+++ cpputest/led_driver_example/src/LedDriver.c
@@ -1,26 +1,53 @@
 #include <stdbool.h>
 #include "LedDriver.h"
 
+enum
+{
+    ALL_LEDS_ON = ~0,
+    ALL_LEDS_OFF = ~ALL_LEDS_ON
+};
+
 static uint16_t* ledsAddress;
+static uint16_t ledsImage;
 
 static uint16_t convertLedNumberToBit(int ledNumber)
 {
     return 1 << (ledNumber -1);
 }
 
+static void updateHardware(void)
+{
+    *ledsAddress = ledsImage;
+}
+
 void LedDriver_Create(uint16_t* address)
 {
     ledsAddress = address;
-    *address = 0;
+    ledsImage = ALL_LEDS_OFF;
+    updateHardware();
 }
 
 void LedDriver_TurnOn(int ledNumber)
 {
-    *ledsAddress |= convertLedNumberToBit(ledNumber);
+    if(ledNumber <= 0 || ledNumber > 16)
+        return;
+
+    ledsImage |= convertLedNumberToBit(ledNumber);
+    updateHardware();
 }
 
 
 void LedDriver_TurnOff(int ledNumber)
 {
-    *ledsAddress = 0;
+    if(ledNumber <= 0 || ledNumber > 16)
+        return;
+
+    ledsImage &= ~(convertLedNumberToBit(ledNumber));
+    updateHardware();
+}
+
+void LedDriver_TurnAllOn(void)
+{
+    ledsImage = ALL_LEDS_ON;
+    updateHardware();
 }
--- cpputest/led_driver_example/test/LedDriverTest.cpp
+++ cpputest/led_driver_example/test/LedDriverTest.cpp
@@ -42,3 +42,50 @@ TEST(LedDriver, TurnOnMultipleLeds)
     LedDriver_TurnOn(8);
     LONGS_EQUAL(0x180, virtualLeds);
 }
+
+TEST(LedDriver, AllOn)
+{
+    LedDriver_TurnAllOn();
+    LONGS_EQUAL(0xFFFF, virtualLeds);
+}
+
+TEST(LedDriver, TurnOffAnyLed)
+{
+    LedDriver_TurnAllOn();
+    LedDriver_TurnOff(8);
+    LONGS_EQUAL(0xFF7F, virtualLeds);
+}
+
+TEST(LedDriver, LedMemoryIsNotReadable)
+{
+    virtualLeds = 0xFFFF;
+    LedDriver_TurnOn(8);
+    LONGS_EQUAL(0x80, virtualLeds);
+}
+
+TEST(LedDriver, UpperAndLowerBounds)
+{
+    LedDriver_TurnOn(1);
+    LedDriver_TurnOn(16);
+    LONGS_EQUAL(0x8001, virtualLeds);
+}
+
+
+TEST(LedDriver, OutOfBoundsChangesNothing)
+{
+    LedDriver_TurnOn(-1);
+    LedDriver_TurnOn(0);
+    LedDriver_TurnOn(17);
+    LedDriver_TurnOn(3141);
+    LONGS_EQUAL(0, virtualLeds);
+}
+
+TEST(LedDriver, OutOfBoundsTurnOffDoesNoHarm)
+{
+    LedDriver_TurnAllOn();
+    LedDriver_TurnOff(-1);
+    LedDriver_TurnOff(0);
+    LedDriver_TurnOff(17);
+    LedDriver_TurnOff(3141);
+    LONGS_EQUAL(0xFFFF, virtualLeds);
+}
